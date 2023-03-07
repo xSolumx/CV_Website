0d1a983b8d233ac38fceb478ad46cb0fe2316d39
Solum <haydaust@gmail.com>
2023-03-07 10:54:25 +0200

    smol changes
    persistent dark mode
    added a project

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
-    $('#dark-mode-toggle').click(function() {
-      $('body').toggleClass('dark-mode');
-      $('.card').toggleClass('dark-mode');
-      $('.btn').toggleClass('btn-dark btn-dark-mode');
-      $('.navbar').toggleClass('navbar-dark bg-dark');
-    });
+  // Check if the user has previously set a preference
+  var isDarkMode = localStorage.getItem('darkMode') === 'true';
+
+  // If dark mode is on, toggle the classes
+  if (isDarkMode) {
+    $('body').addClass('dark-mode');
+    $('.card').addClass('dark-mode');
+    $('.btn').addClass('btn-dark-mode').removeClass('btn-dark');
+    $('.navbar').addClass('navbar-dark bg-dark');
+  }
+
+  // Toggle classes and save the preference to local storage
+  $('#dark-mode-toggle').click(function() {
+    $('body').toggleClass('dark-mode');
+    $('.card').toggleClass('dark-mode');
+    $('.btn').toggleClass('btn-dark-mode btn-dark');
+    $('.navbar').toggleClass('navbar-dark bg-dark');
+    localStorage.setItem('darkMode', $('body').hasClass('dark-mode'));
   });
+});
