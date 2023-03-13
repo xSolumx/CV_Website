f683861bf157ea63ebd14c43d153e6f815850b10
Solum <haydaust@gmail.com>
2023-03-13 11:42:03 +0200

    changed something, idk what

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 $(document).ready(function() {
-  // Check if the user has previously set a preference
-  var isDarkMode = localStorage.getItem('darkMode') === 'true';
+  // Retrieve dark mode preference from local storage
+  var darkMode = localStorage.getItem('darkMode');
 
-  // If dark mode is on, toggle the classes
-  if (isDarkMode) {
+  // Apply dark mode classes to elements if preference exists
+  if (darkMode === 'true') {
     $('body').addClass('dark-mode');
     $('.card').addClass('dark-mode');
-    $('.btn').addClass('btn-dark-mode').removeClass('btn-dark');
+    $('.btn').addClass('btn-dark-mode btn-dark');
     $('.navbar').addClass('navbar-dark bg-dark');
+    $('#dark-mode-toggle').text('Light Mode');
   }
 
   // Toggle classes and save the preference to local storage
   $('#dark-mode-toggle').click(function() {
+    var $btn = $(this);
+    var isDarkMode = $('body').hasClass('dark-mode');
     $('body').toggleClass('dark-mode');
     $('.card').toggleClass('dark-mode');
     $('.btn').toggleClass('btn-dark-mode btn-dark');
     $('.navbar').toggleClass('navbar-dark bg-dark');
+    $btn.text(isDarkMode ? 'Dark Mode' : 'Light Mode');
     localStorage.setItem('darkMode', $('body').hasClass('dark-mode'));
   });
 });
